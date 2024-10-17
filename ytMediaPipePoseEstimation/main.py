8fe5f7b4dd2bab6c96b05552e1e4976a1f85a761
ccfreck <cafreckmann@calebPC>
2024-10-16 23:54:19 -0400

    This commit is up to joint angle calculation

--- ytMediaPipePoseEstimation/main.py
+++ ytMediaPipePoseEstimation/main.py
@@ -8,6 +8,22 @@ mp_pose = mp.solutions.pose
 # SETTING UP THE VIDEO FEED
 cap = cv2.VideoCapture(0)
 
+def calculate_angle(a, b, c):
+    a = np.array(a) # first
+    b = np.array(b) # mid
+    c = np.array(c) # end
+
+    # CONCERN: it is 2D angle estimation not 3D?
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+    angle = np.abs(radians*180.0/np.pi)
+
+    if angle > 180.0:
+        angle = 360 - angle
+    
+    return angle
+
+
+
 # setup mediapipe instance
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
     while cap.isOpened():
@@ -36,10 +52,49 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
                                   mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) #line
                                   )
 
+        # Extract Landmarks
+        if results.pose_landmarks is not None:
+            landmarks = results.pose_landmarks.landmark
+
+            # Get Coordinates
+            left_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x, landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+            left_elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x, landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+            left_wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x, landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+
+            right_shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x, landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+            right_elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x, landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+            right_wrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x, landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+
+            
+            # Calculate Angle
+            left_elbow_angle = calculate_angle(left_shoulder, left_elbow, left_wrist)
+            right_elbow_angle = calculate_angle(right_shoulder, right_elbow, right_wrist)
+
+            # Visualize
+            cv2.putText(image, str(left_elbow_angle), 
+                            tuple(np.multiply(left_elbow, [640, 480]).astype(int)),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                        )
+            
+            
+            cv2.putText(image, str(right_elbow_angle), 
+                            tuple(np.multiply(right_elbow, [640, 480]).astype(int)),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                        )
+            # print(landmarks)
+            # print(len(landmarks))
+            # mp_pose.PoseLandmark.LEFT_SHOULDER.value will return 11
+            # landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value] will return [x,y,z,visibility]
+        else:
+            landmarks = None
+
         cv2.imshow('Mediapipe Feed', image)
+
 
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 
     cap.release()
     cv2.destroyAllWindows()
+
+
